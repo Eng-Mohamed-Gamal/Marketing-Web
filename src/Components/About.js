2a076858c6af4207bd347e2edda73aba1a66eec8
Mohamed gamal <mohamed555asdfgh@gmail.com>
2023-08-12 16:58:11 +0300

    Steps

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-<div className="about" id='About'>
-    <h1 className="cen"><span>About</span> us</h1>
-    <div className="container">
-        <div className="img"><img src="./images/about.png" alt="" /></div>
+    <div className="about" id="About">
+      <h1 className="cen">
+        <span>About</span> us
+      </h1>
+      <div className="container">
+        <div className="img">
+          <img src="./images/about-img.png" alt="" />
+        </div>
+        <div className="con">
+          <h2>big projects start with big dream</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime
+            distinctio incidunt repellendus quod consectetur! Voluptates magni
+            ut doloremque modi laboriosam.
+          </p>
+          <button>learn more</button>
+          <div className="all">
+            <div className="go">
+              <i class="fa-solid fa-award"></i>
+              <p>15 Awards won</p>
+            </div>
+            <div className="go">
+            <i class="fa-solid fa-user"></i>
+              <p>290 satisfied clients</p>
+            </div>
+            <div className="go">
+            <i class="fa-solid fa-diagram-project"></i>
+              <p>500 project done !</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
-  )
+  );
 }
