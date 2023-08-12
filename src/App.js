d32fc7bb8600fed058525b7bb79361b55e2eafd3
Mohamed gamal <mohamed555asdfgh@gmail.com>
2023-08-12 17:50:38 +0300

    Steps

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import Header from './Common/Header';
 import About from './Components/About';
 import Home from './Components/Home';
+import Portfolio from './Components/Portfolio';
 import Services from './Components/Services';
 import './app.css';
 
@@ -12,6 +13,7 @@ function App() {
       <Home></Home>
       <Services></Services>
       <About></About>
+      <Portfolio></Portfolio>
     </div>
   );
 }
---
+++ src/Components/Portfolio.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+export default function Portfolio() {
+  return (
+<div className="portfolio" id='Portfolio' >
+    <h1 className="cen">our <span>portfolio</span> </h1>
+<div className="container">
+    <div className="box">
+        <img src="./images/port-img1.jpg" alt="" />
+        <div className="abso-1"></div>
+        <h1 className='gre'>01</h1>
+        <div className="abso-2"><h1>WebSite Design</h1><p>12 / 8 / 2023</p></div>
+    </div>
+    <div className="box">
+        <img src="./images/port-img2.jpg" alt="" />
+        <div className="abso-1"></div>
+        <h1 className='gre'>02</h1>
+        <div className="abso-2"><h1>WebSite Design</h1><p>12 / 8 / 20233</p></div>
+    </div>
+    <div className="box">
+        <img src="./images/port-img3.jpg" alt="" />
+        <div className="abso-1"></div>
+        <h1 className='gre'>03</h1>
+        <div className="abso-2"><h1>WebSite Design</h1><p>12 / 8 / 2023</p></div>
+    </div>
+    <div className="box">
+        <img src="./images/port-img4.jpg" alt="" />
+        <div className="abso-1"></div>
+        <h1 className='gre'>04</h1>
+        <div className="abso-2"><h1>WebSite Design</h1><p>12 / 8 / 2023</p></div>
+    </div>
+    <div className="box">
+        <img src="./images/port-img5.jpg" alt="" />
+        <div className="abso-1"></div>
+        <h1 className='gre'>05</h1>
+        <div className="abso-2"><h1>WebSite Design</h1><p>12 / 8 / 2023</p></div>
+    </div>
+    <div className="box">
+        <img src="./images/port-img6.jpg" alt="" />
+        <div className="abso-1"></div>
+        <h1 className='gre'>06</h1>
+        <div className="abso-2"><h1>WebSite Design</h1><p>12 / 8 / 2023</p></div>
+    </div>
+</div>
+</div>
+  )
+}
