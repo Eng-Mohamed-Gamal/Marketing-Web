c46eec70acd4ed6aeda71350f965a3d9218759b5
Mohamed gamal <mohamed555asdfgh@gmail.com>
2023-08-12 12:05:54 +0300

    Start

---
+++ src/Common/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+<div className="header">
+    <div className="container">
+        <h1>Marketo</h1>
+        <ul>
+            <li><a href="#Home">Home</a></li>
+            <li><a href="#Services">Services</a></li>
+            <li><a href="#About">About</a></li>
+            <li><a href="#Portfolio">Portfolio</a></li>
+            <li><a href="#Pricing">Pricing</a></li>
+            <li><a href="#Review">Review</a></li>
+            <li><a href="#Contact">Contact</a></li>
+        </ul>
+    </div>
+</div>
+  )
+}
