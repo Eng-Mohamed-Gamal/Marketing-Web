fea45f97961b22829aec50007c83d4aa8f07550a
Mohamed gamal <mohamed555asdfgh@gmail.com>
2023-08-12 14:42:43 +0300

    Steps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
+import Header from './Common/Header';
 import './app.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Header></Header>
     </div>
   );
 }
--- src/Common/Header.js
+++ src/Common/Header.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 export default function Header() {
+
+const barsRef = useRef()
+
+  const Bars = () =>{
+    barsRef.current.classList.toggle("translate")
+    if(barsRef.current.classList.contains("translate")){
+        barsRef.current.classList.add("trans")
+    }else{
+        setTimeout(()=>{
+            barsRef.current.classList.remove("trans")
+        },300)
+    }
+}
   return (
 <div className="header">
     <div className="container">
         <h1>Marketo</h1>
-        <ul>
+        <ul  ref={barsRef} >
             <li><a href="#Home">Home</a></li>
             <li><a href="#Services">Services</a></li>
             <li><a href="#About">About</a></li>
@@ -14,6 +27,7 @@ export default function Header() {
             <li><a href="#Review">Review</a></li>
             <li><a href="#Contact">Contact</a></li>
         </ul>
+        <i class="fa-solid fa-bars" onClick={Bars} ></i>
     </div>
 </div>
   )
